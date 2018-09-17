6077ea34d2f77c8f7a8f0ed0fd54d1d5c102d6d4
Raymond Trang <raymond.d.trang@gmail.com>
2018-09-17 13:44:59 -0400

    Add Pokemon base stats app

--- pokemon_base_stats.py
+++ pokemon_base_stats.py
@@ -5,24 +5,24 @@ def print_stats():
 	real_pokemon = True
 
 	while real_pokemon:
-		pokemon_id = raw_input('Please input Pokemon ID: ')
+		pokemon_id = input('Please input Pokemon ID: ')
 
 		try: 
 			pokemon_url = 'http://pokeapi.co/api/v2/pokemon/' + pokemon_id
 			response = requests.get(pokemon_url)
 			pokemon = response.json()
 
-			print "The base stats for " + str(pokemon['name']) + " are: "
+			print ("The base stats for " + str(pokemon['name']) + " are: ")
 			for stat in pokemon['stats']:
 				if stat_num < len(pokemon['stats']):
-					print pokemon['stats'][stat_num]['stat']['name'] + ": " + \
-					str(pokemon['stats'][stat_num]['base_stat'])
+					print (pokemon['stats'][stat_num]['stat']['name'] + ": " + \
+					str(pokemon['stats'][stat_num]['base_stat']))
 					stat_num += 1
 
 			real_pokemon = False
 
 		except KeyError:
-			print "Please input correct Pokemon ID."
+			print ("Please input correct Pokemon ID.")
 
 print_stats()
 
